e089dc1369b6fea70df30db356124026d11606c1
Naveen <veershetti.naveen@gmail.com>
2022-04-23 20:31:10 +0530

    sample curd on django

---
+++ Hello/home/urls.py
@@ -0,0 +1,31 @@
+"""Hello URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from xml.etree.ElementInclude import include
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+
+urlpatterns = [
+    
+    
+    path('', views.categorylisting, name="categorylisting"),
+    path('create', views.categorycreate, name="categorycreate"),
+    path('inserted', views.categoryaddprocess, name="categoryaddprocess"),
+    path('delete/<int:id>', views.categorydelete, name="categorydelete"),
+    path('edit/<int:id>', views.categoryedit, name="categoryedit"),
+    path('update/', views.categoryupdate, name="categoryupdate"),
+]
